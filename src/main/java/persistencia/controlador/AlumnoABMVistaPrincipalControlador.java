d1472d66deef4a21b9d1a3eaae27a7645fbf753f
AlejandroRey <Alejandro.Catacata@gmail.com>
2018-11-05 15:57:26 -0300

    Historial Notas
    Historial Panel Add

--- src/main/java/persistencia/controlador/AlumnoABMVistaPrincipalControlador.java
+++ src/main/java/persistencia/controlador/AlumnoABMVistaPrincipalControlador.java
@@ -1,6 +1,7 @@
 package persistencia.controlador;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
@@ -46,6 +47,53 @@ public class AlumnoABMVistaPrincipalControlador {
 			}
 		});
 
+		this.vista.getBtnNotas().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnNotas_MousePressed(evt);
+			}
+		});
+		
+		this.vista.getBtnAsistencias().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnAsistencias_MousePressed(evt);
+			}
+		});
+		
+		this.vista.getBtnPagos().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnPagos_MousePressed(evt);
+			}
+		});
+		
+		this.vista.getBtnContactos().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnContactos_MousePressed(evt);
+			}
+		});
+		
+		this.vista.getBtnHomeI().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnHomeI_MousePressed(evt);
+			}
+		});
+		
+		this.vista.getBtnHomeII().addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
+				btnHomeII_MousePressed(evt);
+			}
+		});
+	}	
+
+	private void btnHomeI_MousePressed(MouseEvent evt) {
+		System.out.println("I Press");
+		this.vista.getPanelAbm().setVisible(false);
+		this.vista.getPanelHistorial().setVisible(true);
+	}
+	
+
+	public void btnHomeII_MousePressed(MouseEvent evt) {
+		this.vista.getPanelAbm().setVisible(true);
+		this.vista.getPanelHistorial().setVisible(false);
 	}
 
 	private void btnSeleccionar_MousePressed(MouseEvent evt) {
@@ -109,6 +157,34 @@ public class AlumnoABMVistaPrincipalControlador {
 		this.vista.getMainPanel().repaint();
 	}
 	
+	private void btnNotas_MousePressed(MouseEvent evt) {
+		setColor(this.vista.getBtnNotas());
+		resetColor(new JPanel[] { this.vista.getBtnAsistencias(),
+								  this.vista.getBtnPagos(), 
+								  this.vista.getBtnContactos()});
+
+		
+	}
+	private void btnAsistencias_MousePressed(MouseEvent evt) {
+		setColor(this.vista.getBtnAsistencias());
+		resetColor(new JPanel[] { this.vista.getBtnNotas(),
+								  this.vista.getBtnPagos(), 
+								  this.vista.getBtnContactos()});		
+	}
+	private void btnPagos_MousePressed(MouseEvent evt) {
+		setColor(this.vista.getBtnPagos());
+		resetColor(new JPanel[] { this.vista.getBtnNotas(),
+								  this.vista.getBtnAsistencias(),
+								  this.vista.getBtnContactos()});
+		
+	}
+	private void btnContactos_MousePressed(MouseEvent evt) {
+		setColor(this.vista.getBtnContactos());
+		resetColor(new JPanel[] { this.vista.getBtnNotas(),
+								  this.vista.getBtnAsistencias(), 
+								  this.vista.getBtnPagos()});
+	}
+	
 	private void setColor(JPanel pane) {
 		pane.setBackground(new Color(41, 57, 80));
 	}
@@ -116,6 +192,20 @@ public class AlumnoABMVistaPrincipalControlador {
 	private void resetColor(JPanel[] pane) {
 		for (int i = 0; i < pane.length; i++) {
 			pane[i].setBackground(new Color(23, 35, 51));
+		}
+	}
+	
+	private void setPanelEnabled(JPanel panel, Boolean isEnabled) {
+		panel.setEnabled(isEnabled);
+
+		Component[] components = panel.getComponents();
+
+		for (int i = 0; i < components.length; i++) {
+			if (components[i].getClass().getName() == "javax.swing.JPanel") {
+				setPanelEnabled((JPanel) components[i], isEnabled);
+			}
+
+			components[i].setEnabled(isEnabled);
 		}
 	}
 
---
+++ src/main/java/presentacion/vista/AlumnoHistorialNota.java
@@ -0,0 +1,90 @@
+package presentacion.vista;
+
+import java.awt.Component;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+
+@SuppressWarnings("serial")
+public class AlumnoHistorialNota extends JPanel {
+	
+	private JScrollPane spCursadas;
+	private DefaultTableModel modelCursadas;
+	private JTable tablaCursadas;
+	private String[] nombreColumnasCursadas = {"idFechaCursada", "idCursada", "idSala", "fechaInicio", "fechaFin", "Fecha", "Ini", "Fin","Pre", "Aus"};
+	
+	private JScrollPane spEvaluaciones;
+	private DefaultTableModel modelEvaluaciones;
+	private JTable tablaEvaluaciones;
+	private String[] nombreColumnasEvaluaciones = {"idFechaCursada", "idCursada", "idSala", "fechaInicio", "fechaFin", "Fecha", "Ini", "Fin","Pre", "Aus"};
+	
+	
+	/**
+	 * Create the Panel.
+	 */
+	public AlumnoHistorialNota() {
+		super();
+		
+		this.setBounds(0, 0, 960, 500);
+		this.setLayout(null);
+		inicializar();
+	}
+
+	private void inicializar() {
+		
+		inicializarTablaCursadas();
+		inicializartablaEvaluaciones();
+	}
+	
+	private void inicializarTablaCursadas() {
+		
+		spCursadas = new JScrollPane();
+		spCursadas.setBounds(10, 90, 338, 379);
+		add(spCursadas);
+		
+		modelCursadas = new DefaultTableModel(null, nombreColumnasCursadas);
+		tablaCursadas = new JTable(modelCursadas) {
+			@Override
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+				Component component = super.prepareRenderer(renderer, row, column);
+				int rendererWidth = component.getPreferredSize().width;
+				TableColumn tableColumn = getColumnModel().getColumn(column);
+				tableColumn.setPreferredWidth(Math.max(rendererWidth + getIntercellSpacing().width, tableColumn.getPreferredWidth()));
+				return component;
+			}
+		};
+		
+		tablaCursadas.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		//tblInstructores.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);		
+		spCursadas.setViewportView(tablaCursadas);		
+	}
+	
+	private void inicializartablaEvaluaciones() {
+
+		spEvaluaciones = new JScrollPane();
+		spEvaluaciones.setBounds(367, 90, 338, 379);
+		add(spEvaluaciones);
+
+		modelEvaluaciones = new DefaultTableModel(null, nombreColumnasEvaluaciones);
+		tablaEvaluaciones = new JTable(modelEvaluaciones) {
+			@Override
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+				Component component = super.prepareRenderer(renderer, row, column);
+				int rendererWidth = component.getPreferredSize().width;
+				TableColumn tableColumn = getColumnModel().getColumn(column);
+				tableColumn.setPreferredWidth(
+						Math.max(rendererWidth + getIntercellSpacing().width, tableColumn.getPreferredWidth()));
+				return component;
+			}
+		};
+
+		tablaEvaluaciones.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+		// tblInstructores.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		spEvaluaciones.setViewportView(tablaEvaluaciones);
+	}
+
+}
