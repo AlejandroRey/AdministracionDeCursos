fc8125e82193b3561dd011f4ca67f7678850bcbb
jgpao <jgpao@DESKTOP-JGP>
2018-11-11 16:46:55 -0300

    Creacion de RecadoDTO

---
+++ src/main/java/dto/RecadoDTO.java
@@ -0,0 +1,70 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class RecadoDTO {
+
+	private long idRecado;
+	private long idUsuarioDe;
+	private long idUsuarioPara;
+	private String asunto;
+	private String mensaje;
+	private LocalDateTime fechaHoraEnvio;
+	private boolean visto;
+	
+	public RecadoDTO(long idRecado, long idUsuarioDe, long idUsuarioPara, String asunto, String mensaje,
+			LocalDateTime fechaHoraEnvio, boolean visto) {
+		super();
+		this.idRecado = idRecado;
+		this.idUsuarioDe = idUsuarioDe;
+		this.idUsuarioPara = idUsuarioPara;
+		this.asunto = asunto;
+		this.mensaje = mensaje;
+		this.fechaHoraEnvio = fechaHoraEnvio;
+		this.visto = visto;
+	}
+	
+	public long getIdRecado() {
+		return idRecado;
+	}
+	public void setIdRecado(long idRecado) {
+		this.idRecado = idRecado;
+	}
+	public long getIdUsuarioDe() {
+		return idUsuarioDe;
+	}
+	public void setIdUsuarioDe(long idUsuarioDe) {
+		this.idUsuarioDe = idUsuarioDe;
+	}
+	public long getIdUsuarioPara() {
+		return idUsuarioPara;
+	}
+	public void setIdUsuarioPara(long idUsuarioPara) {
+		this.idUsuarioPara = idUsuarioPara;
+	}
+	public String getAsunto() {
+		return asunto;
+	}
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public LocalDateTime getFechaHoraEnvio() {
+		return fechaHoraEnvio;
+	}
+	public void setFechaHoraEnvio(LocalDateTime fechaHoraEnvio) {
+		this.fechaHoraEnvio = fechaHoraEnvio;
+	}
+	public boolean isVisto() {
+		return visto;
+	}
+	public void setVisto(boolean visto) {
+		this.visto = visto;
+	}
+		
+}
