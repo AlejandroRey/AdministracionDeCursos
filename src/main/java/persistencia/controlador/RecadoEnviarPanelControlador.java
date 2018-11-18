e0805c6435db085821203912bfe59acb57741b48
jgpao <jgpao@DESKTOP-JGP>
2018-11-18 20:40:26 -0300

    Prediccion de usuarios al enviar un recado + aviso de recado visto

--- src/main/java/persistencia/controlador/RecadoEnviarPanelControlador.java
+++ src/main/java/persistencia/controlador/RecadoEnviarPanelControlador.java
@@ -2,9 +2,15 @@ package persistencia.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.swing.JOptionPane;
+
+import com.mxrck.autocompleter.TextAutoCompleter;
 
 import dto.RecadoDTO;
 import dto.UsuarioDTO;
@@ -24,6 +30,47 @@ public class RecadoEnviarPanelControlador implements ActionListener {
 		this.vista.getBtnCancelar().addActionListener(this);
 		this.vista.getBtnEnviar().addActionListener(this);
 		this.vista.getBtnB().addActionListener(this);
+		this.vista.getTxtPara().addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				List<UsuarioDTO> listaTemporalUsuarios = new ArrayList<UsuarioDTO>();
+				usuariosLista.stream()
+						.filter(UsuarioDTO -> UsuarioDTO.getUsuario().contains(vista.getTxtPara().getText()))
+						.collect(Collectors.toList());
+				try {
+					listaTemporalUsuarios.add(usuariosLista.stream()
+							.filter(UsuarioDTO -> UsuarioDTO.getUsuario().contains(vista.getTxtPara().getText()))
+							.collect(Collectors.toList()).get(0));
+					listaTemporalUsuarios.add(usuariosLista.stream()
+							.filter(UsuarioDTO -> UsuarioDTO.getUsuario().contains(vista.getTxtPara().getText()))
+							.collect(Collectors.toList()).get(1));
+					listaTemporalUsuarios.add(usuariosLista.stream()
+							.filter(UsuarioDTO -> UsuarioDTO.getUsuario().contains(vista.getTxtPara().getText()))
+							.collect(Collectors.toList()).get(2));
+				} catch (Exception e2) {
+				}
+
+				// System.out.println(listaTemporalUsuarios.toString());
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+			}
+		});
+
+		TextAutoCompleter textAutoAcompleter = new TextAutoCompleter(this.vista.getTxtPara());
+		for (UsuarioDTO usuarioDTO : usuariosLista) {
+			textAutoAcompleter.addItem(usuarioDTO.getUsuario());
+		}
+
 	}
 
 	@Override
@@ -43,41 +90,51 @@ public class RecadoEnviarPanelControlador implements ActionListener {
 				JOptionPane.showMessageDialog(null, "Destinatario invalido", "Recado", JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
+
 	}
 
 	private void enviarRecado() {
-		if(this.vista.getTxtPara().getText().length() != 0) {
-			if(this.vista.getTextAreaMensaje().getText().length() != 0) {
-				if(this.vista.getTextAsunto().getText().length() == 0) {
-					if(JOptionPane.showConfirmDialog(null, "Enviar recado sin ingresar un asunto?", "Confirmar envio", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0) {
-						if(validarUsuario(this.vista.getTxtPara().getText())) {
+		if (this.vista.getTxtPara().getText().length() != 0) {
+			if (this.vista.getTextAreaMensaje().getText().length() != 0) {
+				if (this.vista.getTextAsunto().getText().length() == 0) {
+					if (JOptionPane.showConfirmDialog(null, "Enviar recado sin ingresar un asunto?", "Confirmar envio",
+							JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == 0) {
+						if (validarUsuario(this.vista.getTxtPara().getText())) {
 							// TODO agregar el id del usuario o nombre del logueado -- 3er parametro
-							RecadoDTO recado = new RecadoDTO(0, 1, getIdUsuarioPara(this.vista.getTxtPara().getText()), this.vista.getTextAsunto().getText(), this.vista.getTextAreaMensaje().getText(), null, true, false);
+							RecadoDTO recado = new RecadoDTO(0, 1, getIdUsuarioPara(this.vista.getTxtPara().getText()),
+									this.vista.getTextAsunto().getText(), this.vista.getTextAreaMensaje().getText(),
+									null, true, false);
 							this.modelo.agregarRecado(recado);
 							limpiarInputs();
-							JOptionPane.showMessageDialog(null, "Recado enviado exitosamente!", "Recado", JOptionPane.INFORMATION_MESSAGE);
+							JOptionPane.showMessageDialog(null, "Recado enviado exitosamente!", "Recado",
+									JOptionPane.INFORMATION_MESSAGE);
 						} else {
-							JOptionPane.showMessageDialog(null, "No se ha podido enviar, destinatario invalido", "Recado", JOptionPane.INFORMATION_MESSAGE);
+							JOptionPane.showMessageDialog(null, "No se ha podido enviar, destinatario invalido",
+									"Recado", JOptionPane.INFORMATION_MESSAGE);
 						}
-					}			
-			} else {
-				if(validarUsuario(this.vista.getTxtPara().getText())) {
-					// TODO agregar el id del usuario o nombre del logueado -- 3er parametro
-					RecadoDTO recado = new RecadoDTO(0, 1, getIdUsuarioPara(this.vista.getTxtPara().getText()), this.vista.getTextAsunto().getText(), this.vista.getTextAreaMensaje().getText(), null, true, false);
-					this.modelo.agregarRecado(recado);
-					limpiarInputs();
-					JOptionPane.showMessageDialog(null, "Recado enviado exitosamente!", "Recado", JOptionPane.INFORMATION_MESSAGE);
+					}
 				} else {
-					JOptionPane.showMessageDialog(null, "No se ha podido enviar, destinatario invalido", "Recado", JOptionPane.INFORMATION_MESSAGE);
+					if (validarUsuario(this.vista.getTxtPara().getText())) {
+						// TODO agregar el id del usuario o nombre del logueado -- 3er parametro
+						RecadoDTO recado = new RecadoDTO(0, 1, getIdUsuarioPara(this.vista.getTxtPara().getText()),
+								this.vista.getTextAsunto().getText(), this.vista.getTextAreaMensaje().getText(), null,
+								true, false);
+						this.modelo.agregarRecado(recado);
+						limpiarInputs();
+						JOptionPane.showMessageDialog(null, "Recado enviado exitosamente!", "Recado",
+								JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						JOptionPane.showMessageDialog(null, "No se ha podido enviar, destinatario invalido", "Recado",
+								JOptionPane.INFORMATION_MESSAGE);
+					}
 				}
-			}	
 			} else {
 				JOptionPane.showMessageDialog(null, "Ingrese un mensaje", "Recado", JOptionPane.INFORMATION_MESSAGE);
 			}
-			} else {
-				JOptionPane.showMessageDialog(null, "Ingrese un destinatario", "Recado", JOptionPane.INFORMATION_MESSAGE);
-			}
-			
+		} else {
+			JOptionPane.showMessageDialog(null, "Ingrese un destinatario", "Recado", JOptionPane.INFORMATION_MESSAGE);
+		}
+
 	}
 
 	private boolean validarUsuario(String usuario) {
